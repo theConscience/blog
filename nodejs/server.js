74d0df16db4976ada3c34043f74eaa6e2777531d
theConscience <vinton@nextmail.ru>
2014-06-11 01:57:33 +0400

    change port to 8080

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,29 +1,16 @@
-//  OpenShift sample Node application
-var http = require('http');
+var http = require("http");
 
-//Get the environment variables we need.
-var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-http.createServer(function (req, res) {
-	var addr = "unknown";
-	var out = "";
-	if (req.headers.hasOwnProperty('x-forwarded-for')) {
-		addr = req.headers['x-forwarded-for'];
-	} else if (req.headers.hasOwnProperty('remote-addr')){
-		addr = req.headers['remote-addr'];
+function start() {
+	function onRequest (request, response) {
+		console.log("Request recieved.");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World!");
+		response.end();
 	}
+	var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+	var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+	http.createServer(onRequest).listen(port,ip);
+	console.log("Server has started.");
+}
 
-	if (req.headers.hasOwnProperty('accept')) {
-		if (req.headers['accept'].toLowerCase() == "application/json") {
-			  res.writeHead(200, {'Content-Type': 'application/json'});
-			  res.end(JSON.stringify({'ip': addr}, null, 4) + "\n");			
-			  return ;
-		}
-	}
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("Welcome to Node.js on OpenShift!\n\n");
-  res.end("Your IP address seems to be " + addr + "\n");
-}).listen(port, ipaddr);
-console.log("Server running at http://" + ipaddr + ":" + port + "/");
+exports.start = start;
