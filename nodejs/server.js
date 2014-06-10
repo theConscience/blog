af43d5f343a34563e8fc6ad01da9e854bc9f5550
theConscience <vinton@nextmail.ru>
2014-06-11 01:44:05 +0400

    add ip

--- nodejs/server.js
+++ nodejs/server.js
@@ -7,8 +7,9 @@ function start() {
 		response.write("Hello World!");
 		response.end();
 	}
-
-	http.createServer(onRequest).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080  );
+	var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+	var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+	http.createServer(onRequest).listen(port,ip);
 	console.log("Server has started.");
 }
 
