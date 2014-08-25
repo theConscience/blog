ac81b3869c298808d1c957c0c8f045cffa96b91f
theConscience <vinton.mail@gmail.com>
2014-08-25 16:17:07 +0400

    server ed

--- nodejs/server.js
+++ nodejs/server.js
@@ -7,9 +7,10 @@ function start() {
 		response.write("Hello World!");
 		response.end();
 	}
-	var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-	var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-	http.createServer(onRequest).listen(port,ip);
+	var port =  process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_PORT || 3030;
+	var ip = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+	http.createServer(onRequest).listen(port,ip, function() {
+		console.log('%s: Node server of Conscience started on %s:%d ...', new Date() ), ip, port);
 	console.log("Server has started.");
 }
 
